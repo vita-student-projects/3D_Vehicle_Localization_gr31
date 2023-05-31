8a904597fbbe169be532ecd0bd434a8aa1b0c57e
Maxime LELIEVRE <maxime.lelievre@epfl.ch>
2023-06-01 00:08:54 +0200

    change file .sh to run

--- code_VisualDet3D/train_test_for_scitas.sh
+++ code_VisualDet3D/train_test_for_scitas.sh
@@ -13,17 +13,15 @@
 nvidia-smi
 
 module load gcc/8.4.0-cuda python/3.7.7 cuda/10.2.89
+
 source ~/venvs/venv-g31_pytorch112/bin/activate
-cd ~/DLAV_project_gr31_1st_train/DLAV_project_gr31
+
 python3 scripts/imdb_precompute_3d.py --config="config/config_file.py"
-echo "ATTENTION, NOUS PASSONS AU TESTING"
+
 python3 scripts/imdb_precompute_test.py --config="config/config_file.py"
-echo "ATTENTION, LE TESTING EST FINI"
-#echo "ATTENTION, NOUS PASSONS AU TRAINING"
-echo "ICI LE CUDA_VISIBLE_DEVICES=0"
-#CUDA_VISIBLE_DEVICES=0 python3 scripts/train.py --config="config/config_file.py" --experiment_name="experiment_1"
-echo "ATTENTION, LE TRAINING EST FINI lol pas de train en fait"
-echo "ATTENTION, NOUS ne faisons pas L'EVAL VALIDATION"
-#CUDA_VISIBLE_DEVICES=0 python3 scripts/eval.py --config="config/config_file.py" --gpu=0 --checkpoint_path="visualDet3D/workdirs/Mono3D/checkpoint/GroundAware_pretrained.pth" --split_to_test="validation"
-echo "ATTENTION, NOUS PASSONS à L'EVAL TEST"
+
+CUDA_VISIBLE_DEVICES=0 python3 scripts/train.py --config="config/config_file.py" --experiment_name="experiment_1"
+
+CUDA_VISIBLE_DEVICES=0 python3 scripts/eval.py --config="config/config_file.py" --gpu=0 --checkpoint_path="visualDet3D/workdirs/Mono3D/checkpoint/GroundAware_pretrained.pth" --split_to_test="validation"
+
 CUDA_VISIBLE_DEVICES=0 python3 scripts/eval.py --config="config/config_file.py" --gpu=0 --checkpoint_path="visualDet3D/workdirs/Mono3D/checkpoint/GroundAware_pretrained.pth" --split_to_test="test"  
